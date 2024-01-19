380807c4a449009c98c33c90b3c470d708233c09
Michael Goh <mikegoh1997@gmail.com>
2024-01-18 23:10:23 -0800

    Deploy test page

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text style={{fontSize: 24}}>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
